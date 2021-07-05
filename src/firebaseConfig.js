1a7ab29891d6d03566f3214ad9ecd45ca7195e52
sanjidaafrin25 <sanjidaafrinmou70@gmail.com>
2021-07-05 13:25:17 +0600

    complete website

---
+++ src/firebaseConfig.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBcL0jDHVm038S1QVjfnWi5VQJwI1h_RHw",
+  authDomain: "ema-jhon-with-auth-284b1.firebaseapp.com",
+  projectId: "ema-jhon-with-auth-284b1",
+  storageBucket: "ema-jhon-with-auth-284b1.appspot.com",
+  messagingSenderId: "246776071378",
+  appId: "1:246776071378:web:082a3aa0056a34af40d90b"
+};
+export default firebaseConfig;
